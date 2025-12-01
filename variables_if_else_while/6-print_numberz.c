2268323257f519dc8304d34b210f41deaf54c63b
psevdoner <davidabdurahmanov38@gmail.com>
2025-12-01 07:03:27 +0000

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0'); /* rəqəmi simvol kimi çap edirik */
+
+	putchar('\n'); /* yeni sətir */
+
+	return (0);
+}
