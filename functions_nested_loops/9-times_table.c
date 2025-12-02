50300d68b26182e0d2c64212725287d73cfbb1fd
psevdoner <davidabdurahmanov38@gmail.com>
2025-12-02 20:15:22 +0000

    creating tame table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * times_table - prints the 9 times table, starting with 0
@@ -8,33 +8,25 @@
 void times_table(void)
 {
 	int i, j, result;
-	char digit;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			result = i * j;
-			
-			/* Print result with proper formatting */
-			if (j != 0)
+
+			if (j == 0)
 			{
-				write(1, ", ", 2);
+				printf("%d", result);
+			}
+			else
+			{
+				printf(", ");
 				if (result < 10)
-					write(1, " ", 1);
+					printf(" ");
+				printf("%d", result);
 			}
-			
-			/* Print tens digit if needed */
-			if (result >= 10)
-			{
-				digit = (result / 10) + '0';
-				write(1, &digit, 1);
-			}
-			
-			/* Print ones digit */
-			digit = (result % 10) + '0';
-			write(1, &digit, 1);
 		}
-		write(1, "\n", 1);
+		printf("\n");
 	}
 }
