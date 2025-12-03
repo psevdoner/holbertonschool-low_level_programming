722278253cbaefe683271ac2079fb648829b27e2
psevdoner <davidabdurahmanov38@gmail.com>
2025-12-03 01:22:27 +0000

    Advanced Task2: prints an integer using _putchar

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using _putchar
+ * @n: integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n == -2147483648)
+		{
+			_putchar('2');
+			n = 147483648;
+		}
+		else
+			n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
+
