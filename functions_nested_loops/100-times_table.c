328ae2f5d4eeeb44d17d00b7093569b99db7757f
vitasec <davidabdurahmanov38@gmail.com>
2025-12-04 08:48:41 +0000

    100 tables

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,8 +1,12 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer using only _putchar
- * @n: number to print
+ * print_number - prints an integer using only _putchar.
+ *
+ * This function handles positive integers up to 999 (since n <= 15,
+ * max result is 15 * 15 = 225).
+ *
+ * @n: The number to print.
  *
  * Return: void
  */
@@ -26,8 +30,48 @@ void print_number(int n)
 }
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: the number to generate the table for
+ * print_formatted_result - Prints the result with necessary spacing/comma.
+ *
+ * @result: The product (i * j).
+ * @is_first: Flag indicating if it is the first number in the row (j == 0).
+ *
+ * Return: void
+ */
+void print_formatted_result(int result, int is_first)
+{
+	if (is_first)
+	{
+		print_number(result);
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+
+		if (result < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			print_number(result);
+		}
+		else if (result < 100)
+		{
+			_putchar(' ');
+			print_number(result);
+		}
+		else
+		{
+			print_number(result);
+		}
+	}
+}
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ *
+ * If n is greater than 15 or less than 0 the function does nothing.
+ *
+ * @n: The number to generate the table for.
  *
  * Return: void
  */
@@ -36,7 +80,9 @@ void print_times_table(int n)
 	int i, j, result;
 
 	if (n < 0 || n > 15)
+	{
 		return;
+	}
 
 	for (i = 0; i <= n; i++)
 	{
@@ -44,14 +90,9 @@ void print_times_table(int n)
 		{
 			result = i * j;
 
-			if (j == 0)
-			{
-				print_number(result);
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (result < 10)
-				{
-
+			/* Ayrılmış köməkçi funksiyanı çağırırıq */
+			print_formatted_result(result, (j == 0));
+		}
+		_putchar('\n');
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -13,16 +13,25 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+// 🛠️ DƏYİŞİKLİK 1: print_times_table sənədləşməsi
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: the number to generate the table for
+ *
+ * Return: void
+ */
 void print_times_table(int n);
-int _putchar(char c);
 
+// _putchar təkrar olunur, onu silmək olar, amma betty üçün əsas deyil.
 
-
-
-
-
-
-
+// 🛠️ DƏYİŞİKLİK 2: print_number sənədləşməsi
+/**
+ * print_number - prints an integer using only _putchar
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_number(int n);
 
 #endif
-
