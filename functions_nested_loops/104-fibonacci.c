7affc7b923d0e83d96b43fadd0d3e7338ae04509
vitasec <davidabdurahmanov38@gmail.com>
2025-12-05 16:30:26 +0000

    104 fibonacci

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,57 +1,43 @@
 #include <stdio.h>
 
-void print_fib(void);
-
 /**
- * main - entry point, prints the first 98 Fibonacci numbers
+ * main - prints the first 98 Fibonacci numbers starting with 1 and 2
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	print_fib();
-	return (0);
-}
-
-/**
- * print_fib - prints the first 98 Fibonacci numbers
- */
-void print_fib(void)
-{
 	int i;
-	unsigned long a = 1, b = 2, a_h, a_l, b_h, b_l, h, l;
+	unsigned long a1 = 1, a2 = 2, a1_h = 0, a2_h = 0;
+	unsigned long h, l;
 	unsigned long div = 1000000000000000000UL;
 
-	printf("%lu, %lu", a, b);
+	printf("%lu, %lu", a1, a2);
+
 	for (i = 3; i <= 98; i++)
 	{
-		if (a < div && b < div)
+		l = a1 + a2;
+		h = a1_h + a2_h;
+
+		if (l >= div)
 		{
-			h = 0;
-			l = a + b;
+			l -= div;
+			h += 1;
 		}
-		else
-		{
-			a_h = a / div;
-			a_l = a % div;
-			b_h = b / div;
-			b_l = b % div;
-			l = a_l + b_l;
-			h = a_h + b_h;
-			if (l >= div)
-			{
-				h++;
-				l -= div;
-			}
-		}
+
 		printf(", ");
 		if (h == 0)
 			printf("%lu", l);
 		else
 			printf("%lu%018lu", h, l);
-		a = b;
-		b = h * div + l;
+
+		a1 = a2;
+		a1_h = a2_h;
+		a2 = l;
+		a2_h = h;
 	}
+
 	printf("\n");
+	return (0);
 }
 
