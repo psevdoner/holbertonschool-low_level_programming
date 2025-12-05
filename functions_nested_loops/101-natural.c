cde83056fb0f3a712ce4476d3348c5009e14a2fa
vitasec <davidabdurahmanov38@gmail.com>
2025-12-05 15:50:58 +0000

    101 is tame table

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,52 +1,36 @@
 #include "main.h"
 
-/**
- * main - runs the program
- *
- * Return: always 0
- */
 int main(void)
 {
-	int sum, i;
+    int sum = 0, i;
 
-	sum = 0;
-	for (i = 1; i < 1024; i++)
-	{
-		if (i % 3 == 0 || i % 5 == 0)
-			sum += i;
-	}
+    for (i = 1; i < 1024; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    }
 
-	print_int(sum);
+    print_int(sum);
+    _putchar('\n');
 
-	_putchar('\n');
-
-	return (0);
+    return (0);
 }
 
-
-/**
- * print_int - prints integers
- * @n: integer to print
- *
- * Return: void
- */
 void print_int(int n)
 {
-	int temp, divisor;
+    int temp = n;
+    int divisor = 1;
 
-	temp = n;
-	divisor = 1;
+    while (temp > 9)
+    {
+        divisor *= 10;
+        temp /= 10;
+    }
 
-	while (temp > 9)
-	{
-		divisor *= 10;
-		temp /= 10;
-	}
-
-	while (divisor >= 1)
-	{
-		_putchar((n / divisor) % 10 + '0');
-		divisor /= 10;
-	}
+    while (divisor >= 1)
+    {
+        _putchar((n / divisor) % 10 + '0');
+        divisor /= 10;
+    }
 }
 
