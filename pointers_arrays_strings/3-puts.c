7485d611692a0ea4b2d9c4bc3304362eec5352c8
vitasec <davidabdurahmanov38@gmail.com>
2025-12-08 08:58:50 +0000

    task 03  new line, to stdout.

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: Pointer to the string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
