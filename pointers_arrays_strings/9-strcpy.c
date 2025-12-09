f64bf2929e3d19e1e68aced3709aa2d88458a551
vitasec <davidabdurahmanov38@gmail.com>
2025-12-09 09:22:50 +0000

    task 09 solve of all problems

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,42 +1,25 @@
 #include "main.h"
 
 /**
- * puts_half - Prints the second half of a string, followed by a new line.
- * @str: The string to be processed and printed.
+ * _strcpy - strcpy
+ * @src: str to copy
+ * @dest: to
  *
- * Description: Calculates length and prints the second half.
+ * Return: the pointer to dest
  */
-void puts_half(char *str)
+char *_strcpy(char *dest, char *src)
 {
-	int len = 0;
-	int start_index;
+	char *start;
 
-	/* Sətrin uzunluğunu tapmaq */
-	while (str[len] != '\0')
+	start = dest;
+	while (*src)
 	{
-		len++;
+		*dest = *src;
+		src++;
+		dest++;
 	}
 
-	/* Uzunluğun cüt olub-olmamasına görə başlanğıc indeksini hesabla */
-	if (len % 2 == 0)
-	{
-		/* Cüt: Məsələn, uzunluq 10 olsa, başlanğıc 10 / 2 = 5 */
-		start_index = len / 2;
-	}
-	else
-	{
-		/* Tək: Məsələn, uzunluq 9 olsa, başlanğıc (9 + 1) / 2 = 5 */
-		/* Bu, n = (L + 1) / 2 qaydasına uyğundur. */
-		start_index = (len + 1) / 2;
-	}
-
-	/* İkinci yarı simvolları çap etmək */
-	while (str[start_index] != '\0')
-	{
-		_putchar(str[start_index]);
-		start_index++;
-	}
-
-	/* Yeni sətir əlavə etmək */
-	_putchar('\n');
+	*dest = '\0';
+	return (start);
 }
+
