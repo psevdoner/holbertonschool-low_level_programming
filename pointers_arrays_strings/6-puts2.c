0ffd091d46eeed295f5071bb27192ad98b89069d
vitasec <davidabdurahmanov38@gmail.com>
2025-12-09 08:58:54 +0000

    task 06 new

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,36 +1,21 @@
 #include "main.h"
 
 /**
- * puts_half - Prints the second half of a string, followed by a new line.
- * @str: The string to be processed and printed.
+ * puts2 - Prints every other character of a string, starting with the first.
+ * @str: The string to be processed.
  *
- * Description: Calculates the string length and determines the
- * starting index for the second half based on whether the length
- * is even or odd. It then prints the characters from that index.
+ * Description: Prints characters at index 0, 2, 4, 6, etc.,
+ *
+ * followed by a new line.
  */
-void puts_half(char *str)
+void puts2(char *str)
 {
-	int len = 0;
-	int start_index;
+	int i = 0;
 
-	while (str[len] != '\0')
+	while (str[i] != '\0')
 	{
-		len++;
-	}
-
-	if (len % 2 == 0)
-	{
-		start_index = len / 2;
-	}
-	else
-	{
-		start_index = (len + 1) / 2;
-	}
-
-	while (str[start_index] != '\0')
-	{
-		_putchar(str[start_index]);
-		start_index++;
+		_putchar(str[i]);
+		i += 2;
 	}
 
 	_putchar('\n');
