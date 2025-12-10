1ed5e048fa9a40a586da27d8761a9c99fbaee955
vitasec <davidabdurahmanov38@gmail.com>
2025-12-10 05:21:34 +0000

    task 03: compare 2 strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings like strcmp
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: negative, 0, or positive difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return (0);
+}
+
