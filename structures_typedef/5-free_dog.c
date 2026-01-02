96ac9b476fc60ab82ae2cfc11677b0b6e747732d
vitasec <davidabdurahmnaov38@gmail.com>
2026-01-02 15:50:28 +0000

    finish

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*free_dog - it  ucunu ayrilan yaddasi azad edir
+*@d: azad edilecen itin unvani
+*
+*Description: birinic icindeki elementleri silir
+*sonra ozunu silir
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d)
+}
