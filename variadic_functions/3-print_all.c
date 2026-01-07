af1047d84d1a28b4260608dabb32e6d401e31690
vitasec <davidabdurahmanov38@gmail.com>
2026-01-07 15:38:45 +0000

    prints anything.

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_all - her bir sey  cap eden funksiya
+*@format: tiplerin siyahisi
+*
+*/
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *str, *sep = "";
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		sep = ", ";
+		i++;
+	}
+
+	printf("\n");
+	va_end(args);
+}
